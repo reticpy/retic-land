db54f540a89e7b993092d0e4dbea0085cab944dd
Braian Staimer <Braian Staimer>
2020-09-06 20:44:13 -0600

    + title

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 import Layout from "components/Layout";
 import SEO from "components/Seo";
+import { Typography } from "@material-ui/core";
 
 export default function Home({}) {
   return (
     <Layout>
       <SEO title="All posts" />
+      <Typography variant="h1">Retic</Typography>
       <Link href={"/manual"} as={`/manual`}>
         <a className="text-4xl font-bold text-orange-600 font-display">
           Manual
